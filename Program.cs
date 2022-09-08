2491765217291dd4f275ec2b7577d28bc1cc1948
Daria-Russia <111522488+Daria-Russia@users.noreply.github.com>
2022-09-08 18:20:20 +0300

    Task 19 committed

--- Program.cs
+++ Program.cs
@@ -1,37 +1,18 @@
-﻿//There is a separate branch for each task
+﻿
 //Task 19
 
-//string? number = Console.ReadLine();
+string? number = Console.ReadLine();
 
-//void CheckingNumber(string number){
-//  if (number[0]==number[4] || number[1]==number[3]){
-//    Console.WriteLine("yes");
-//  }
-//  else Console.WriteLine("no");
-//}
+void CheckingNumber(string number){
+if (number[0]==number[4] || number[1]==number[3]){
+Console.WriteLine("yes");
+ }
+  else Console.WriteLine("no");
+}
 
-//if (number!.Length == 5){
-//  CheckingNumber(number);
-//}
-//else Console.WriteLine("Wrong number");
+if (number!.Length == 5){
+CheckingNumber(number);
+}
+else Console.WriteLine("Wrong number");
 
-//Task 21
-//int x1 = Convert.ToInt32(Console.ReadLine());
-//int y1 = Convert.ToInt32(Console.ReadLine());
-//int z1 = Convert.ToInt32(Console.ReadLine());
-//int x2 = Convert.ToInt32(Console.ReadLine());
-//int y2 = Convert.ToInt32(Console.ReadLine());
-//int z2 = Convert.ToInt32(Console.ReadLine());
 
-//int k = (x1-x2);
-//int l = (y1-y2);
-//int m = (z1-z2);
-
-//Console.Writeline(Math.Sqrt(k*k+l*l+m*m));
-
-//Task 23
-
-int n = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i<=n; i++)
-
-Console.Write(Math.Pow(i, 3)+" "); 
